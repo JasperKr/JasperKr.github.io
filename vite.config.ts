ce31ef14305baf406f40aa0ed794b8202a64e851
Jasper <j.krocke@outlook.com>
2025-02-20 10:49:10 +0100

    yep

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import path from 'path'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-      postcss: {
-        plugins: [tailwindcss, autoprefixer],
-      },
-    },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, './src'),
-      crypto: 'crypto-browserify'
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  }
 })
